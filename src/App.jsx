45274f650eaa3bdb51145fc80824245c9bb5f340
Oluwatobi Tella-Ndanusa <tellajames63@gmail.com>
2024-01-18 18:29:12 +0100

    First Import and Export

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import {HomeHeader} from './components/header/Header.jsx'
 function App() {
 
   return (
     <>
       <div>
+        <HomeHeader/>
         <h1>Hello World!</h1>
         <h2>This is my First React App..!</h2>
       </div>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+export function HomeHeader() {
+    return (
+        <>
+        <div>
+            <h1>My name is Oluwatobi Tella-Ndanusa</h1>
+            <br />
+            <h2>I am an aspiring Full-Stack Developer, who is currently a Front-End Developer</h2>
+        </div>
+        </>
+    )
+}

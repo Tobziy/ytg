e3a41e2d95d4dc48639c2064a8b5e3341b48f5ad
Oluwatobi Tella-Ndanusa <tellajames63@gmail.com>
2024-01-19 12:40:22 +0100

    Prop method 2 but no output

--- src/components/props/testprop1.jsx
+++ src/components/props/testprop1.jsx
@@ -1,21 +1,35 @@
-import {PropBody} from './testprop.jsx';
-import tailwindImg from '../../assets/tailwind.svg'
+import  {My_Stack}  from './testprop2';
+import '../style/card.css';
 export function DaddyProp() {
     return (
-        <>
-        <section>
+    
+        <main>
+        <section id="core-concepts">
             <h2>My Stack</h2>
             <ul>
-              <PropBody 
-               title="Tailwind CSS "
-               description ="I use Tailwind CSS to design my websites"
-               img={tailwindImg}
-               />
-              <PropBody/>
-              <PropBody/>
-              <PropBody/>
+              <My_Stack 
+              title = {My_Stack[0].title}
+              description = {My_Stack[0].description}
+              img = {My_Stack[0].img}
+              />
+              <My_Stack 
+              title = {My_Stack[1].title}
+              description = {My_Stack[1].description}
+              img = {My_Stack[1].img}
+              />
+              <My_Stack 
+              title = {My_Stack[2].title}
+              description = {My_Stack[2].description}
+              img = {My_Stack[2].img}
+              />
+              <My_Stack 
+              title = {My_Stack[3].title}
+              description = {My_Stack[3].description}
+              img = {My_Stack[3].img}
+              />
             </ul>
         </section>
-        </>
+        </main>
+        
     )
 }
---
+++ src/components/props/testprop2.jsx
@@ -0,0 +1,31 @@
+import reactImg from '../../assets/react.svg';
+import tailwindImg from '../../assets/tailwind.svg';
+import htmlImg from '../../assets/html.svg';
+import nextjsImg from '../../assets/nextjs.svg';
+
+export const My_Stack = [
+  {
+    image: reactImg,
+    title: 'React',
+    description:
+      'JavaScript library for building user interfaces',
+  },
+  {
+    image: tailwindImg,
+    title: 'Tailwind',
+    description:
+      'Utility-first CSS framework',
+  },
+  {
+    image: htmlImg,
+    title: 'HTML',
+    description:
+      'HTML is the standard markup language for creating Web pages',
+  },
+  {
+    image: nextjsImg,
+    title: 'Next.js',
+    description:
+      'React framework that simplifies buiding dynamic and fast web applications',
+  },
+];
